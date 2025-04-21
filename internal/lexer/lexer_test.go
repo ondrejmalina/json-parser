480afa0a8cf083119fd15cdf71c9ed3be72e855b
Ondrej Malina <ondrej.malina@outlook.com>
2025-04-21 19:22:16 +0200

    Add test for lex of string token

---
+++ internal/lexer/lexer_test.go
@@ -0,0 +1,16 @@
+package lexer
+
+import (
+	"testing"
+)
+
+func TestMatchToken(t *testing.T) {
+
+	testRunes := []rune("\"ab\"")
+	l := Lexer{runes: testRunes}
+
+	tokString := Token{STRING, 0}
+	if tok := l.GetToken(); tok != tokString {
+		t.Errorf("String not parsed")
+	}
+}
